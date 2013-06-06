d5108d449301b10b21c400a48093cd657cfaa0be
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2013-06-06 18:05:59 +1000

    Awesome: working main()
    But, no apparent difference between cases and controls!

--- cafe_main.py
+++ cafe_main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 # standard library imports
-import sys
 import argparse
+import itertools as it
 
 # dependency library imports
-import mahotas
+from mahotas import io
+from matplotlib import pyplot as plt
 
 # local imports
 import cafe
@@ -23,5 +24,17 @@ def main():
 
     Use `cafe -h` or `cafe --help` for options.
     """
-    parser.parse_args()
-    pass
+    args = parser.parse_args()
+    test_images = [io.imread(fn) for fn in args.test_cases]
+    test_rnapii = it.chain(*[cafe.rnapii_centromere_vs_chromatin(im)
+                             for im in test_images])
+    control_images = [io.imread(fn) for fn in args.controls]
+    control_rnapii = it.chain(*[cafe.rnapii_centromere_vs_chromatin(im)
+                                for im in control_images])
+
+    plt.boxplot(list(test_rnapii) + list(control_rnapii))
+    plt.savefig('boxplot.pdf', bbox_inches='tight')
+
+
+if __name__ == '__main__':
+    main()
