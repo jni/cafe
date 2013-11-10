e44647b314e33fc28d76fe460609aed0e367f5f2
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2013-11-10 18:25:17 +1100

    Initial interactive quantification module

---
+++ interactive.py
@@ -0,0 +1,44 @@
+import sys
+import cafe
+import numpy as np
+from skimage import viewer
+from skimage.viewer.plugins.overlayplugin import OverlayPlugin
+from skimage.viewer.widgets import Slider
+
+
+def encode(im, **kwargs):
+    if kwargs.has_key('alpha'):
+        a = kwargs['alpha']
+        del kwargs['alpha']
+    else:
+        a = 100
+    ov = cafe.encode_centro_telomeres_multichannel(im, **kwargs)
+    return (a * (ov > 0) + int(a * 0.42) * ov).astype(np.uint8)
+
+
+class CentroPlugin(OverlayPlugin):
+    def __init__(self, *args, **kwargs):
+        super(CentroPlugin, self).__init__(image_filter=encode, **kwargs)
+        
+    def attach(self, image_viewer):
+        self.add_widget(Slider('alpha', 0, 100, value=100, value_type='int'))
+        self.add_widget(Slider('centro_min_size', 0, 100,
+                                value=14, value_type='int'))
+        self.add_widget(Slider('centro_radius', 0, 100,
+                                value=10, value_type='int'))
+        self.add_widget(Slider('telo_offset', -256, 255, value=0))
+        self.add_widget(Slider('telo_adapt_radius', 0, 101,
+                                value=49, value_type='int'))
+        self.add_widget(Slider('telo_open_radius', 0, 20,
+                                value=4, value_type='int'))
+        super(CentroPlugin, self).attach(image_viewer)
+
+
+def main():
+    rgbs = np.imread(sys.argv[1])
+    v = viewer.ImageViewer(rgbs)
+    v += CentroPlugin()
+    overlay = v.show()[0][0]
+
+if __name__ == '__main__':
+    main()
