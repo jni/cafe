93ce03ee15fbec90af5a40f7262fb34025a607b7
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-06-05 14:57:40 +1000

    Update quantification to produce dataframe

--- trf1.py
+++ trf1.py
@@ -8,23 +8,79 @@ def threshold(im):
     return im > filters.threshold_otsu(im)
 
 
-@tz.curry
-def trf_quantify(im, prop='mean_intensity', normalise=None):
-    # normalise can be None, 'pre', 'post'
+def trf_quantify(im):
+    """Quantify the TRF1 blobs in an image.
+
+    Parameters
+    ----------
+    im : array of shape (M, N[, P], 3)
+        The input image. The red (0th) channel should contain TRF1
+        signal, while the blue (2nd) channel should contain chromatin
+        signal.
+
+    Returns
+    -------
+    props : list of list of float
+        The desired properties measured for each blob in the image:
+        blob size, raw mean intensity, raw total intensity, raw max
+        intensity, 
+        and eccentricity.
+    """
     trf = im[..., 0]
-    chrom = im[..., 2]
     objs = nd.label(threshold(trf))[0]
-    if normalise == 'pre':
-        trf = trf.astype(float) / (chrom + 1)
-    if normalise is None or normalise == 'pre':
-        chrom = np.ones_like(trf)
-    props = [getattr(p, prop) / getattr(q, prop)
-             for p, q in zip(measure.regionprops(objs, trf),
-                             measure.regionprops(objs, chrom))]
-    return props
+    trfprops = measure.regionprops(objs, trf)
+    # non-intensity features
+    sizes = [p.area for p in trfprops]
+    eccen = [p.eccentricity for p in trfprops]
+    # unnormalised properties (raw)
+    rmean = [p.mean_intensity for p in trfprops]
+    rtotl = [s * m for s, m in zip(sizes, rmean)]
+    rmaxs = [p.max_intensity for p in trfprops]
+    # post-normalised properties
+    chrom = im[..., 2]
+    chrprops = measure.regionprops(objs, chrom)
+    means = [p.mean_intensity / q.mean_intensity
+             for p, q in zip(trfprops, chrprops)]
+    total = [s * m for s, m in zip(sizes, means)]
+    maxes = [p.max_intensity / q.max_intensity
+             for p, q in zip(trfprops, chrprops)]
+    # pre-normalised properties
+    trf = trf.astype(float) / (chrom + 1)
+    trfprops = measure.regionprops(objs, trf)
+    nmean = [p.mean_intensity for p in trfprops]
+    ntotl = [s * m for s, m in zip(sizes, rmean)]
+    nmaxs = [p.max_intensity for p in trfprops]
+    return list(zip(sizes, rmean, rtotl, rmaxs,
+                           means, total, maxes,
+                           nmean, ntotl, nmaxs, eccen))
 
 
 def scatter(kd, control, colors=['orange', 'blue'], **kwargs):
+    """Show a jittered scatterplot of the measurements.
+
+    Parameters
+    ----------
+    kd : list of list of float
+        The list of `trf_quantify` results for all AUKB knockdown
+        images in the dataset. (Each result is itself a list.)
+    control : list of list of float
+        The list of `trf_quantify` results for all control images in
+        the dataset.
+    colors : list of two matplotlib colorspecs, optional
+        The colors corresponding to AUKB-KD (0) and control (1) data
+        points on the scatterplot.
+    
+    Additional Parameters
+    ---------------------
+    **kwargs : keyword arguments
+        Additional keyword arguments passed directly to
+        ``plt.scatter``.
+
+    Returns
+    -------
+    fig : matplotlib axes
+        The returned value from the call to ``plt.scatter``.
+    """
     xs = list(tz.concat([i + 0.2 * np.random.randn(n)
                          for i, n in enumerate(map(len, kd + control))]))
     color_vector = ([colors[0]] * sum(map(len, kd)) +
